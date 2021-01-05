23c832aa261f68eec352766ffee0dc10966c06e2
Ak <sherbetsmoon@gmail.com>
2021-01-05 14:55:53 +0900

    lesson05

--- mamazon/urls.py
+++ mamazon/urls.py
@@ -5,4 +5,5 @@ app_name = 'mamazon'
 
 urlpatterns = [
     path('', views.Home.as_view(), name='home'),
+    path('products/', views.ProductListView.as_view(), name='product-list'),
 ]
--- mamazon/views.py
+++ mamazon/views.py
@@ -1,5 +1,21 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
+from .models import Product
 
 
 class Home(TemplateView):
     template_name = 'mamazon/home.html'
+
+
+class ProductListView(ListView):
+    model = Product
+    template_name = "mamazon/list.html"
+
+    def get_queryset(self):
+        queryset = Product.objects.all()
+        if 'query' in self.request.GET:
+            qs = self.request.GET['query']
+            queryset = queryset.filter(name__contains=qs)
+        # get_querysetのスニペットで表示された２行と return queryset
+        # queryset = super(CLASS_NAME, self).get_queryset()
+        # queryset = queryset # TODO
+        return queryset
