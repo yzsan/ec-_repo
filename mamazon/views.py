a294b4e8cbfaebf6b8606264938f909a6b2625be
Ak <sherbetsmoon@gmail.com>
2021-01-09 12:57:36 +0900

    lesson06

--- mamazon/views.py
+++ mamazon/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import TemplateView, ListView
+from django.views.generic import TemplateView, ListView, DetailView
 from .models import Product
 
 
@@ -19,3 +19,8 @@ class ProductListView(ListView):
         # queryset = super(CLASS_NAME, self).get_queryset()
         # queryset = queryset # TODO
         return queryset
+
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = "mamazon/detail.html"
